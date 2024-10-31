60659a7542a08181f957aa5d019be02ff073488d
Moe <github@m03.ca>
2024-10-31 11:32:01 -0700

    add controllers for mount manager ui

--- web/assets/js/super.mountManager.js
+++ web/assets/js/super.mountManager.js
@@ -59,5 +59,54 @@ $(document).ready(function(){
             })
         })
     }
+    function addMount(form) {
+        return new Promise((resolve,reject) => {
+            // const { sourceTarget, localPath, mountType, options } = form;
+            $.post(superApiPrefix + $user.sessionKey + '/mountManager/mount', form,function(data){
+                resolve(data)
+            })
+        })
+    }
+    function removeMount(localPath) {
+        return new Promise((resolve,reject) => {
+            $.post(superApiPrefix + $user.sessionKey + '/mountManager/removeMount',{
+                localPath
+            },function(data){
+                resolve(data)
+            })
+        })
+    }
+    newMountForm.submit(async function(e){
+        e.preventDefault();
+        const form = newMountForm.serializeObject();
+        const response = await addMount(form);
+        const notify = {
+            title: lang['Mount Added'],
+            type: 'success'
+        }
+        if(!response.ok){
+            notify.title = lang['Failed to Add Mount']
+            notify.text = response.error
+            notify.type = 'danger'
+        }
+        new PNotify(notify)
+        return false;
+    });
+    theTable.on('click','.delete', async function(e){
+        const el = $(this).parents('[row-mounted]')
+        const mountId = el.attr('row-mounted');
+        const theMount = loadedMounts[mountId]
+        const localPath = theMount.mountPoint
+        const response = await removeMount(localPath);
+        if(response.ok){
+            el.remove()
+        }else{
+            new PNotify({
+                title: lang['Failed to Remove Mount'],
+                text: lang['See System Logs'],
+                type: 'danger'
+            })
+        }
+    })
     loadMounts()
 })
